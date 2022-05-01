96792877bad54d515f412d97f14b12c9be2fd5c4
jrix58 <Propelledvisualsllc@gmail.com>
2022-05-01 17:25:20 -0400

    website update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,83 +1,84 @@
-require('dotenv').config({
-  path: `.env`,
-});
+require(`dotenv`).config()
 
-const config = require('./config')
-
-const pathPrefix = config.pathPrefix === '/' ? '' : config.pathPrefix
+const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
 module.exports = {
-  pathPrefix: config.pathPrefix,
   siteMetadata: {
-    siteUrl: config.siteUrl + pathPrefix,
-    pathPrefix,
-    title: config.siteTitle,
-    titleAlt: config.siteTitleAlt,
-    description: config.siteDescription,
-    logo: config.siteLogo,
-    headline: config.siteHeadline,
-    siteLanguage: config.siteLanguage,
-    ogLanguage: config.ogLanguage,
-    author: config.author,
-    twitter: config.userTwitter,
-    facebook: config.ogSiteName,
+    // You can overwrite values here that are used for the SEO component
+    // You can also add new values here to query them like usual
+    // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-jodie/gatsby-config.js
+    siteTitle: `Propelled Visuals`,
+    siteTitleAlt: `Propelled Visuals - Drone Company`,
+    siteHeadline: `Propelled Visuals - Drone Company`,
+    siteUrl: `https://propelledvisuals.com`,
+    siteDescription: `Image-heavy photography portfolio with colorful accents & customizable pages. Includes adaptive image grids powered by CSS grid and automatic image integration into projects.`,
+    siteLanguage: `en`,
+    siteImage: `/banner.jpg`,
+    author: `Propelled Visuals`,
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-styled-components',
-    'gatsby-plugin-typescript',
-    'gatsby-transformer-yaml',
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: `@lekoarts/gatsby-theme-jodie`,
+      // See the theme's README for all available options
       options: {
-        name: 'projects',
-        path: `${__dirname}/content/projects`,
+        navigation: [
+          { name: `Projects`, slug: `/projects` },
+          { name: `About`, slug: `/about` },
+        ],
       },
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: `gatsby-omni-font-loader`,
       options: {
-        name: 'config',
-        path: `${__dirname}/config`,
+        enableListener: true,
+        preconnect: [`https://fonts.gstatic.com`],
+        interval: 300,
+        timeout: 30000,
+        // If you plan on changing the font you'll also need to adjust the Theme UI config to edit the CSS
+        // See: https://github.com/LekoArts/gatsby-themes/tree/main/examples/jodie#changing-your-fonts
+        web: [
+          {
+            name: `Work Sans`,
+            file: `https://fonts.googleapis.com/css2?family=Work+Sans:wght@400..700&display=swap`,
+          },
+        ],
       },
     },
+    `gatsby-plugin-sitemap`,
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'images',
-        path: `${__dirname}/src/images`,
+        name: `Propelled Visuals`,
+        short_name: `Propelled Visuals`,
+        description: `Propelled`,
+        start_url: `/`,
+        background_color: `black`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        theme_color: `black`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
       },
     },
-    {
-      resolve: 'gatsby-source-instagram',
+    `gatsby-plugin-gatsby-cloud`,
+    shouldAnalyseBundle && {
+      resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
       options: {
-        access_token: process.env.ACCESS_TOKEN,
-        instagram_id: process.env.BUSINESS_ID,
-      }
-    },
-    {
-      resolve: 'gatsby-plugin-google-analytics',
-      options: {
-        trackingId: config.googleAnalyticsID,
+        analyzerMode: `static`,
+        reportFilename: `_bundle.html`,
+        openAnalyzer: false,
       },
     },
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sitemap',
-    {
-      resolve: 'gatsby-plugin-manifest',
-      options: {
-        name: config.siteTitle,
-        short_name: config.siteTitleShort,
-        description: config.siteDescription,
-        start_url: config.pathPrefix,
-        background_color: config.backgroundColor,
-        theme_color: config.themeColor,
-        display: 'standalone',
-        icon: 'src/favicon.png',
-      },
-    },
-    'gatsby-plugin-offline',
-    'gatsby-plugin-netlify',
-  ],
+  ].filter(Boolean),
 }
---
+++ src/@lekoarts/gatsby-theme-jodie/icons/logo.jsx
@@ -0,0 +1,11 @@
+import * as React from "react"
+import logo from '../../../../static/plogo.jpg'
+import './logo.css'
+
+const Logo = () => (
+  <img src={logo}  
+  className="logo"
+  alt="logo" />
+)
+
+export default Logo
